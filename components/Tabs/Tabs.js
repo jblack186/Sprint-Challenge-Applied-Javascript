4408a27fcd47bfa0b3a2a2d58cda2542189db209
Jamison Blackwell <blackwellj1040@gmail.com>
2019-05-10 22:30:05 -0400

    finished project

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -15,7 +15,7 @@ class TabLink {
      this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`);
      }
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-     this.cards = Array.from(this.cards).map(tabElement => new TabCard());
+     this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
      this.tabElement.addEventListener('click', () => { this.selectTab();
@@ -26,7 +26,6 @@ class TabLink {
 
     // Select all elements with the .tab class on them
      const tabs = document.querySelectorAll(".tab");
-    console.log(tabs);
     // Iterate through the NodeList removing the .active-tab class from each element
     tabs.forEach(function(tabs) {
       tabs.classList.remove('active-tab');
@@ -34,13 +33,14 @@ class TabLink {
 
     // Select all of the elements with the .card class on them
      const cards = document.querySelectorAll('.card');
+    
 
     // Iterate through the NodeList setting the display style each one to 'none'
      cards.forEach(function(cards) {
        cards.style.display='none';
      })
-    
-    // Add a class of ".active-tab" to this.tabElement
+
+          // Add a class of ".active-tab" to this.tabElement
     this.tabElement = this.tabElement.classList.add('active-tab');
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
@@ -51,8 +51,7 @@ class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
      this.cardElement = cardElement;
-     console.log(this.cardElement);
-
+console.log(this.cardElement);
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
